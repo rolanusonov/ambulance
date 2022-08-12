2ddc01d6bea5fd22c0d53514da2cedf2c8ee36ed
rolanusonov <rolanusonov031209@gmail.com>
2022-08-12 23:01:47 +0600

    initi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+ import './App.css';
+import './styles/styles.scss'
+import Home from "./home/Home"
+
+ function App() {
+  return (
+
+      <div>
+          <Home/>
+       </div>
+  );
+}
+
+export default App;
---
+++ src/conpanents/Hero.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import logo from "../img/image 8.png";
+import med from "../img/Med Skill.png";
+import circle from "../img/Frame 1 (2).png";
+import ambulance from "../img/image 21.png";
+import traced1 from "../img/image 15 (Traced).png";
+import traced2 from "../img/image 16 (Traced).png";
+import traced3 from "../img/image 17 (Traced).png";
+import traced4 from "../img/image 18 (Traced).png";
+
+const Hero = () => {
+    return (
+        <div id="hero">
+            <div className="container">
+                <div className="hero">
+                    <div className="hero__title">
+                        <div className="hero__title__logo">
+                            <img src={logo} alt=""/>
+                            <img src={med} alt=""/>
+                        </div>
+                        <div className="hero__title__Title">
+                            <h1>Скорая медицинская помощь 24/7 </h1>
+                        </div>
+                        <div className="hero__title__row">
+                            <ul className="hero__title__row__ul">
+                                <li className="hero__title__row__ul__li">Снятие интоксикации</li>
+                                <li className="hero__title__row__ul__li">Санавиация</li>
+                                <li className="hero__title__row__ul__li">Дежурство на мероприятиях</li>
+                                <li className="hero__title__row__ul__li">Вызов узких специалистов</li>
+
+                            </ul>
+                        </div>
+                        <button className="hero__title__btn">
+                            <a className="hero__title__btn__tel" href="tel:+996501482929">Вызвать</a>
+
+                        </button>
+                    </div>
+                    <div className="hero__img">
+                        <img className="hero__img__circle" src={circle} alt=""/>
+                        <img className="hero__img__ambulance" src={ambulance} alt=""/>
+
+                    </div>
+                </div>
+                <div className="day">
+                    <div className="day__div">
+                        <img className="day__div__img12" src={traced1} alt=""/>
+                        <h3 className="day__div__h312">Работаем день и ночь</h3>
+                    </div>
+                    <div  className="day__div">
+                        <img className="day__div__img12" src={traced2} alt=""/>
+                        <h3 className="day__div__h312">Профессиональная команда</h3>
+                    </div>
+                    <div  className="day__div">
+                        <img className="day__div__img12" src={traced3} alt=""/>
+                        <h3 className="day__div__h312">Гарантия на качество сделанной работы</h3>
+                    </div>
+                    <div  className="day__div">
+                        <img className="day__div__img12" src={traced4} alt=""/>
+                        <h3 className="day__div__h312">Мы любим пунктуальность</h3>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
